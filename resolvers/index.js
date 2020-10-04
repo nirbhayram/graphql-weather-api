09a44e68ee0da1e73df89d3868f62eba3fa9911f
Nirbhay <da201501068@gmail.com>
2020-10-04 12:44:03 +0530

    added date field

--- resolvers/index.js
+++ resolvers/index.js
@@ -2,162 +2,84 @@ const axios = require("axios");
 const { UserInputError } = require("apollo-server");
 
 const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.KEY}`;
-const GROUP_API = `https://api.openweathermap.org/data/2.5/group?appid=${process.env.KEY}`;
 const WEATHER_API_DETAIL = (lat, lon) => (`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${process.env.KEY}&units=metric`)
 
 const resolvers = {
 
-  Query: {
+	Query: {
 
-    getCityByName: async (obj, args, context, info) => {
-      // name is required | country and config are optional
-      const { name, country, config } = args;
-      let url = `${WEATHER_API}&q=${name}`;
+		getCityByName: async (obj, args, context, info) => {
+			// name is required | country and config are optional
+			const { name, country, config } = args;
+			let url = `${WEATHER_API}&q=${name}`;
 
-      // Add other fields if possible
-      if (country) url = url + `,${country}`;
-      if (config && config.units) url = url + `&units=${config.units}`;
-      if (config && config.lang) url = url + `&lang=${config.lang}`;
+			// Add other fields if possible
+			if (country) url = url + `,${country}`;
+			if (config && config.units) url = url + `&units=${config.units}`;
+			if (config && config.lang) url = url + `&lang=${config.lang}`;
 
-      try {
-        const { data } = await axios.get(url);
+			try {
+				const { data } = await axios.get(url);
 
-        // By default, any invalid country code is ignored by the API
-        // In this case, the API returns data for any city that matches the name
-        // To prevent false positives, an error is thrown if country codes don't match
-        if (country && country.toUpperCase() !== data.sys.country) {
-          throw new UserInputError("Country code was invalid", {
-            invalidArgs: { country: country },
-          });
-        }
+				if (country && country.toUpperCase() !== data.sys.country) {
+					throw new UserInputError("Country code was invalid", {
+						invalidArgs: { country: country },
+					});
+				}
 
-        const response = await axios.get(WEATHER_API_DETAIL(data.coord.lat, data.coord.lon))
-        // console.log(response)
-        const current = {
-          temperature: response.data.current.temp,
-          icon: response.data.current.weather[0].icon,
-          main: response.data.current.weather[0].main,
-          sunrise: response.data.current.sunrise,
-          sunset: response.data.current.sunset,
-          pop: response.data.daily[0].pop,
-          uv: response.data.daily[0].uvi,
-          dewDrops: response.data.daily[0].dew_point,
-          windSpeed: response.data.current.wind_speed,
-          humidity: response.data.current.humidity
-        }
-        // console.log(current)
-        const hourData = [];
-        let i=0;
-        while(i<10){
-          hourData.push({
-            time: response.data.hourly[i].dt,
-            icon: response.data.hourly[i].weather[0].icon,
-            temperature: response.data.hourly[i].temp
-          })
-          i=i+1;
-        }
-        const dailyData = [];
-        i=0;
-        while(i<8){
-          dailyData.push({
-            date: response.data.daily[i].dt,
-            icon: response.data.daily[i].weather[0].icon,
-            minTemperature: response.data.daily[i].temp.min,
-            maxTemperature: response.data.daily[i].temp.max
-          })
-          i = i+1;
-        }
+				const response = await axios.get(WEATHER_API_DETAIL(data.coord.lat, data.coord.lon))
+				const current = {
+					temperature: response.data.current.temp,
+					icon: response.data.current.weather[0].icon,
+					main: response.data.current.weather[0].main,
+					sunrise: response.data.current.sunrise,
+					sunset: response.data.current.sunset,
+					pop: response.data.daily[0].pop,
+					uv: response.data.daily[0].uvi,
+					dewDrops: response.data.daily[0].dew_point,
+					windSpeed: response.data.current.wind_speed,
+					humidity: response.data.current.humidity
+				}
+				const hourData = [];
+				let i = 0;
+				while (i < 10) {
+					hourData.push({
+						time: response.data.hourly[i].dt,
+						icon: response.data.hourly[i].weather[0].icon,
+						temperature: response.data.hourly[i].temp
+					})
+					i = i + 1;
+				}
+				const dailyData = [];
+				i = 0;
+				while (i < 8) {
+					dailyData.push({
+						date: response.data.daily[i].dt,
+						icon: response.data.daily[i].weather[0].icon,
+						minTemperature: response.data.daily[i].temp.min,
+						maxTemperature: response.data.daily[i].temp.max
+					})
+					i = i + 1;
+				}
+				console.log(`${data.name} : ${new Date(data.dt*1000).toString()}`)
 
-        return {
-          id: data.id,
-          name: data.name,
-          country: data.sys.country,
-          coord: data.coord,
-          hourData:hourData,
-          dailyData:dailyData,
-          current:current,
-          weather: {
-            summary: {
-              title: data.weather[0].main,
-              description: data.weather[0].description,
-              icon: data.weather[0].icon,
-            },
-            temperature: {
-              actual: data.main.temp,
-              feelsLike: data.main.feels_like,
-              min: data.main.temp_min,
-              max: data.main.temp_max,
-            },
-            wind: {
-              speed: data.wind.speed,
-              deg: data.wind.deg,
-            },
-            clouds: {
-              all: data.clouds.all,
-              visibility: data.visibility,
-              humidity: data.main.humidity,
-            },
-            timestamp: data.dt,
-          },
-        };
-      } catch (e) {
-        console.log(e);
-        return null;
-      }
-    },
+				return {
+					name: data.name,
+					dt:data.dt,
+					hourData: hourData,
+					dailyData: dailyData,
+					current: current
+				};
+			} catch (e) {
+				console.log(e);
+				throw new Error('Something went wrong!')
+			}
+		}
 
-    getCityById: async (obj, args, context, info) => {
-      // id is required (can be string or array) | config is optional
-      const { id, config } = args;
-      let url = `${GROUP_API}&id=${id.join(",")}`;
-
-      // Add other fields if possible
-      if (config && config.units) url = url + `&units=${config.units}`;
-      if (config && config.lang) url = url + `&lang=${config.lang}`;
-
-      try {
-        const { data } = await axios.get(url);
-        const cityList = data.list.map((city) => {
-          return {
-            id: city.id,
-            name: city.name,
-            country: city.sys.country,
-            coord: city.coord,
-            weather: {
-              summary: {
-                title: city.weather[0].main,
-                description: city.weather[0].description,
-                icon: city.weather[0].icon,
-              },
-              temperature: {
-                actual: city.main.temp,
-                feelsLike: city.main.feels_like,
-                min: city.main.temp_min,
-                max: city.main.temp_max,
-              },
-              wind: {
-                speed: city.wind.speed,
-                deg: city.wind.deg,
-              },
-              clouds: {
-                all: city.clouds.all,
-                visibility: city.visibility,
-                humidity: city.main.humidity,
-              },
-              timestamp: city.dt,
-            },
-          };
-        });
-        return cityList;
-      } catch (e) {
-        return null;
-      }
-    },
-
-  },
+	}
 
 };
 
 module.exports = {
-  resolvers,
+	resolvers,
 };
--- typeDefs/index.js
+++ typeDefs/index.js
@@ -2,149 +2,108 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
 
-  type DailyData{
-    date: Float
-    icon: String
-    minTemperature: Float
-    maxTemperature: Float
-  }
+	type DailyData{
+		date: Float
+		icon: String
+		minTemperature: Float
+		maxTemperature: Float
+	}
 
-  type Current{
-    temperature: Float
-    icon: String
-    main: String
-    sunrise: Float
-    sunset: Float
-    pop: Float
-    uv: Float
-    dewDrops: Float
-    windSpeed: Float
-    humidity: Float
-  }
+	type Current{
+		temperature: Float
+		icon: String
+		main: String
+		sunrise: Float
+		sunset: Float
+		pop: Float
+		uv: Float
+		dewDrops: Float
+		windSpeed: Float
+		humidity: Float
+	}
 
-  type HourData {
-    time: Float
-    icon: String
-    temperature: Float
-  }
+	type HourData {
+		time: Float
+		icon: String
+		temperature: Float
+	}
 
-  type City {
-    id: ID
-    name: String
-    country: String
-    coord: Coordinates
-    weather: Weather
-    current: Current
-    hourData: [HourData]
-    dailyData: [DailyData]
-  }
+	type City {
+		name: String
+		dt:Float
+		current: Current
+		hourData: [HourData]
+		dailyData: [DailyData]
+	}
 
-  type Coordinates {
-    lon: Float
-    lat: Float
-  }
+	input ConfigInput {
+		units: Unit
+		lang: Language
+	}
 
-  type Summary {
-    title: String
-    description: String
-    icon: String
-  }
+	type Query {
+		getCityByName(name: String!, country: String, config: ConfigInput): City
+	}
 
-  type Temperature {
-    actual: Float
-    feelsLike: Float
-    min: Float
-    max: Float
-  }
+	enum Unit {
+		metric
+		imperial
+		kelvin
+	}
 
-  type Wind {
-    speed: Float
-    deg: Int
-  }
-
-  type Clouds {
-    all: Int
-    visibility: Int
-    humidity: Int
-  }
-
-  type Weather {
-    summary: Summary
-    temperature: Temperature
-    wind: Wind
-    clouds: Clouds
-    timestamp: Int
-  }
-
-  input ConfigInput {
-    units: Unit
-    lang: Language
-  }
-
-  type Query {
-    getCityByName(name: String!, country: String, config: ConfigInput): City
-    getCityById(id: [String!], config: ConfigInput): [City]
-  }
-
-  enum Unit {
-    metric
-    imperial
-    kelvin
-  }
-
-  enum Language {
-    af
-    al
-    ar
-    az
-    bg
-    ca
-    cz
-    da
-    de
-    el
-    en
-    eu
-    fa
-    fi
-    fr
-    gl
-    he
-    hi
-    hr
-    hu
-    id
-    it
-    ja
-    kr
-    la
-    lt
-    mk
-    no
-    nl
-    pl
-    pt
-    pt_br
-    ro
-    ru
-    sv
-    se
-    sk
-    sl
-    sp
-    es
-    sr
-    th
-    tr
-    ua
-    uk
-    vi
-    zh_cn
-    zh_tw
-    zu
-  }
+	enum Language {
+		af
+		al
+		ar
+		az
+		bg
+		ca
+		cz
+		da
+		de
+		el
+		en
+		eu
+		fa
+		fi
+		fr
+		gl
+		he
+		hi
+		hr
+		hu
+		id
+		it
+		ja
+		kr
+		la
+		lt
+		mk
+		no
+		nl
+		pl
+		pt
+		pt_br
+		ro
+		ru
+		sv
+		se
+		sk
+		sl
+		sp
+		es
+		sr
+		th
+		tr
+		ua
+		uk
+		vi
+		zh_cn
+		zh_tw
+		zu
+	}
 `;
 
 module.exports = {
-  typeDefs,
+	typeDefs,
 };
